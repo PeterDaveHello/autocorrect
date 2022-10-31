87465f07db201012947e9a9b23ee7aa0791d13a2
Jason Lee <huacnlee@gmail.com>
2022-11-01 01:29:21 +0800

    Disable lint_for, swifth_bridge current is not support Vec<SharedStruct>.

--- autocorrect-swift/src/lib.rs
+++ autocorrect-swift/src/lib.rs
@@ -1,30 +1,26 @@
-use autocorrect::LintResult;
-
 #[swift_bridge::bridge]
 mod ffi {
-    #[swift_bridge::bridge(swift_repr = "struct")]
-    pub struct LintResult {
-        raw: String,
-        filepath: String,
-        lines: Vec<LineResult>,
-        error: String,
-    }
+    //     #[swift_bridge::bridge(swift_repr = "struct")]
+    //     pub struct LintResult {
+    //         raw: String,
+    //         filepath: String,
+    //         lines: Vec<LineResult>,
+    //         error: String,
+    //     }
 
-    #[swift_bridge::bridge(swift_repr = "struct")]
-    pub struct LineResult {
-        old: String,
-        new: String,
-        line: usize,
-        col: usize,
-        severity: usize,
-    }
+    //     #[swift_bridge::bridge(swift_repr = "struct")]
+    //     pub struct LineResult {
+    //         old: String,
+    //         new: String,
+    //         line: usize,
+    //         col: usize,
+    //         severity: usize,
+    //     }
 
-    extern "Swift" {
-        type AutoCorrect;
-
+    extern "Rust" {
         fn format(input: &str) -> String;
         fn format_for(input: &str, filepath: &str) -> String;
-        fn lint_for(input: &str, filepath: &str) -> LintResult;
+        // fn lint_for(input: &str, filepath: &str) -> LintResult;
     }
 }
 
@@ -36,23 +32,23 @@ fn format_for(input: &str, filepath: &str) -> String {
     autocorrect::format_for(input, filepath).out
 }
 
-fn lint_for(input: &str, filepath: &str) -> ffi::LintResult {
-    let result = autocorrect::lint_for(input, filepath);
+// fn lint_for(input: &str, filepath: &str) -> ffi::LintResult {
+//     let result = autocorrect::lint_for(input, filepath);
 
-    ffi::LintResult {
-        raw: result.raw.clone(),
-        filepath: result.filepath.clone(),
-        lines: result
-            .lines
-            .iter()
-            .map(|l| ffi::LineResult {
-                old: l.old.clone(),
-                new: l.new.clone(),
-                line: l.line,
-                col: l.col,
-                severity: l.severity as usize,
-            })
-            .collect(),
-        error: result.error.clone(),
-    }
-}
+//     ffi::LintResult {
+//         raw: result.raw.clone(),
+//         filepath: result.filepath.clone(),
+//         lines: result
+//             .lines
+//             .iter()
+//             .map(|l| ffi::LineResult {
+//                 old: l.old.clone(),
+//                 new: l.new.clone(),
+//                 line: l.line,
+//                 col: l.col,
+//                 severity: l.severity as usize,
+//             })
+//             .collect(),
+//         error: result.error.clone(),
+//     }
+// }
